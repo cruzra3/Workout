06fb73638d97a5457e6b3641c80d0d8e048acdc7
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-12 10:32:49 -0400

    Test the app displays the fragment with placeholders for title and description.

--- app/src/main/java/com/raco/workout/zMilestones.java
+++ app/src/main/java/com/raco/workout/zMilestones.java
@@ -3,5 +3,5 @@ package com.raco.workout;
 public class zMilestones {
     //Test creation of 2 activities with first activity with a button with intent to display
     //  second activity
-    //
+    //Test the app displays the fragment with placeholders for title and description
 }
