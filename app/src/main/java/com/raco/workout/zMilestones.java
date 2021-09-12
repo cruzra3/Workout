83619d7a73068f281cfff727ae9a3f8cd31bca8f
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-12 10:10:55 -0400

    Test creation of 2 activities with first activity with a button with intent to display second activity

---
+++ app/src/main/java/com/raco/workout/zMilestones.java
@@ -0,0 +1,7 @@
+package com.raco.workout;
+
+public class zMilestones {
+    //Test creation of 2 activities with first activity with a button with intent to display
+    //  second activity
+    //
+}
