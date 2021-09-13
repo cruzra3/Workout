28dfd5875214f12bfa9376b8b75766039a9b2162
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-13 17:35:15 -0400

    //Test the app displays the actual workout title and description instesd of placeholders.

--- app/src/main/java/com/raco/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/raco/workout/WorkoutDetailFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class WorkoutDetailFragment extends Fragment {
 
@@ -19,6 +20,19 @@ public class WorkoutDetailFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getName());
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
     }
 
     public void setWorkoutId(long id){
--- app/src/main/java/com/raco/workout/zMilestones.java
+++ app/src/main/java/com/raco/workout/zMilestones.java
@@ -4,4 +4,6 @@ public class zMilestones {
     //Test creation of 2 activities with first activity with a button with intent to display
     //  second activity
     //Test the app displays the fragment with placeholders for title and description
+    //Test the app displays the actual workout title and description instesd of
+    //  placeholders
 }
