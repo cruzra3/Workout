11b69e48a2e576a4db4834d2ffc79f7a4e39106b
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-14 15:21:19 -0400

    Test whenever an item in the WorkoutListFragment list view gets clicked, we’ll replace the contents of the frame layout with a new instance of WorkoutDetailFragment that displays details of the correct workout.

--- app/src/main/java/com/raco/workout/MainActivity.java
+++ app/src/main/java/com/raco/workout/MainActivity.java
@@ -1,6 +1,7 @@
 package com.raco.workout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,8 +18,20 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void itemClicked(long id) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int) id);
-        startActivity(intent);
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if (fragmentContainer != null) {
+            //Add the fragment to the FrameLayout
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            details.setWorkoutId(id);
+            ft.replace(R.id.fragment_container, details);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.addToBackStack(null);
+            ft.commit();
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int) id);
+            startActivity(intent);
+        }
     }
 }
