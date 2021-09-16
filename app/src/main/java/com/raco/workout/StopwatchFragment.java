c6c9cf765f1d5dcb3b36a610b0529183d4476e09
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-16 08:05:44 -0400

    Create a stopwatch fragment with corresponding layout.  Put the stopwatch fragment inside the activity temp xml layout.  Add string resources.

---
+++ app/src/main/java/com/raco/workout/StopwatchFragment.java
@@ -0,0 +1,121 @@
+package com.raco.workout;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class StopwatchFragment extends Fragment implements View.OnClickListener {
+    private int seconds = 0;
+    private boolean running;
+    private boolean wasRunning;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_stopwatch, container, false);
+        runTimer(layout);
+        Button startButton = (Button) layout.findViewById(R.id.start_button);
+        startButton.setOnClickListener(this);
+        Button stopButton = (Button) layout.findViewById(R.id.stop_button);
+        stopButton.setOnClickListener(this);
+        Button resetButton = (Button) layout.findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(this);
+        return layout;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.start_button:
+                onClickStart();
+                break;
+            case R.id.stop_button:
+                onClickStop();
+                break;
+            case R.id.reset_button:
+                onClickReset();
+                break;
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+
+    //Start the stopwatch running when the Start button is clicked.
+    private void onClickStart() {
+        running = true;
+    }
+
+    //Stop the stopwatch running when the Stop button is clicked.
+    private void onClickStop() {
+        running = false;
+    }
+
+    //Reset the stopwatch when the Reset button is clicked.
+    private void onClickReset() {
+        running = false;
+        seconds = 0;
+    }
+
+    //Sets the number of seconds on the timer.
+    private void runTimer(View view) {
+        final TextView timeView = (TextView) view.findViewById(R.id.time_view);
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+            }
+        }, 1000);
+    }
+}
